9d5e9162e3af7e925f98f5d07af8b8cd3f855ff9
sweet3831 <april42817@naver.com>
2018-08-20 20:54:44 +0900

    int
    int float

---
+++ 02-data-type-1.py
@@ -0,0 +1,28 @@
+# 정수 int - integer
+print(1+1)
+
+# 소수 float
+print(1.5+2.1)
+
+# 문자 str - string
+print("hello")
+
+print("hello"+"1")
+
+# 정수 소수 간 변경
+print(4/2)
+print(4//2)
+print(int(4/2))
+print(float(2))
+
+#문자열 숫자 간 변경
+#문자
+print("1")
+#숫자
+print(int("1"))
+
+#1.5를 정수로 변경하기!
+
+print(int("1.5"))
+
+#결과 1이 나온다. 혹은 에러?
